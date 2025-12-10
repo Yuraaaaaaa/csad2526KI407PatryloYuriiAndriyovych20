a20a7279cd847c11381be26837562ce37654d46c
Yurii Patrylo <patrulo.yuriy@gmail.com>
2025-12-10 02:46:00 +0200

    Feature: Added CI-scripts

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/User/Desktop/APKS/csad2526KI407PatryloYuriiAndriyovych20
+# Build directory: C:/Users/User/Desktop/APKS/csad2526KI407PatryloYuriiAndriyovych20/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "C:/Users/User/Desktop/APKS/csad2526KI407PatryloYuriiAndriyovych20/build/unit_tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/User/Desktop/APKS/csad2526KI407PatryloYuriiAndriyovych20/CMakeLists.txt;53;add_test;C:/Users/User/Desktop/APKS/csad2526KI407PatryloYuriiAndriyovych20/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
